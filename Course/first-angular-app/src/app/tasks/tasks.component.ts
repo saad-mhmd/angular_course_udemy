548881819157b2f0008acd53acef6a978e1aa125
Saad-OMP <mohammad.saad@openmindprojects.net>
2024-11-05 21:00:28 +0100

    54

--- Course/first-angular-app/src/app/tasks/tasks.component.ts
+++ Course/first-angular-app/src/app/tasks/tasks.component.ts
@@ -1,17 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-tasks/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
+
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+
+  isAddingTask = false;
+  
   tasks = [
     {
       id: 't1',
@@ -44,6 +49,20 @@ export class TasksComponent {
 
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    // my solution
+    // if (!this.isAddingTask) {
+    //   this.isAddingTask = true;
+    // } else {
+    //   this.isAddingTask = false;
+    // }
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
   }
 
 }
