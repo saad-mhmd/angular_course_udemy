b85cdcc61128774f5a88de6a6070a4201964cab4
Saad-OMP <mohammad.saad@openmindprojects.net>
2024-11-05 14:00:24 +0100

    49

--- Course/first-angular-app/src/app/user/user.component.ts
+++ Course/first-angular-app/src/app/user/user.component.ts
@@ -1,16 +1,6 @@
 import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
-// type User = {
-//   id: string;
-//   avatar: string;
-//   name: string;
-// }
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -20,6 +10,7 @@ interface User {
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
