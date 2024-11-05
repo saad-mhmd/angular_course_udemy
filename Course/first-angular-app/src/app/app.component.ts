7727e2921580b13274a91f5c76fe1bb20b7063f2
Saad-OMP <mohammad.saad@openmindprojects.net>
2024-11-05 12:03:04 +0100

    commit 41

---
+++ Course/first-angular-app/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
+
+
+@Component({
+  selector: 'app-root', // tells Angular to render this component in the <app-root> tag
+  standalone: true,
+  imports: [HeaderComponent, UserComponent, TasksComponent],
+  templateUrl: './app.component.html', // tells Angular to use this HTML file as the template
+  styleUrl: './app.component.css', // tells Angular to use this CSS file as the styles
+})
+export class AppComponent {
+  users = DUMMY_USERS;
+  selectUserId = 'u1';
+
+  get selectUser() {
+    return this.users.find((user) => user.id === this.selectUserId);
+  }
+
+  onSelectUser(id: string) {
+    this.selectUserId = id;
+  }
+
+}
---
+++ Course/first-angular-app/src/app/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header', // convension is to use at least 2 words separated by a dash
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
+})
+export class HeaderComponent {}
